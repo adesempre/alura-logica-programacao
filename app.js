a0f40e9e3cd86f6b98d926053325ce1e166eab1b
Thiago de Assis Teixeira <Ser_DaLua@hotmail.com>
2024-01-26 17:48:54 -0300

    aula 01 - desafio

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 29;
 let chute = prompt('Escolha um número entre 1 e 30');
@@ -5,3 +6,18 @@ let chute = prompt('Escolha um número entre 1 e 30');
 if (numeroSecreto == chute) {
     console.log('Isso aí! Você descobriu o número secreto (29)');
 }
+
+/*
+alert('Boas vindas ao nosso site!');
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+nome = prompt('Qual o seu nome?');
+idade = prompt('Qual a sua idade?');
+if (idade > 18) {
+    alert('Pode tirar a habilitação!')
+}
+*/
