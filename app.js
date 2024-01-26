b757e5773dfb1afbbf874dc0cb5e6826a56fa109
Thiago de Assis Teixeira <Ser_DaLua@hotmail.com>
2024-01-26 18:03:15 -0300

    aula 02.04 - vscode ext: live server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 29;
-console.log(numeroScreto);
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 30');
 // se o chute for igual ao número secreto
 if (numeroSecreto == chute) {
