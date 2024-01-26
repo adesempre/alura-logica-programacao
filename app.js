80b925a67b31c95800fc63a09ee9138c72abc19d
Thiago de Assis Teixeira <Ser_DaLua@hotmail.com>
2024-01-26 17:35:46 -0300

    aula 05 - if, console.log, intro dev tools

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 29;
-prompt('Escolha um número entre 1 e 30');
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == chute) {
+    console.log('Isso aí! Você descobriu o número secreto (29)');
+}
